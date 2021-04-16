bd631ee503151b94181ee99cf1cad3b8bd4b0c30
thislexic <benvillabroza1999@gmail.com>
2021-04-16 17:06:25 +0800

    created separate window for adding products

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import sys
 from PyQt5.QtWidgets import *
 from PyQt5.QtSql import *
 from ui.main_design import *
+from ui.add_product_design import *
 
 
 class DlgMain(QMainWindow, Ui_MainWindow):
@@ -53,12 +54,35 @@ class DlgMain(QMainWindow, Ui_MainWindow):
                 row = self.table_product.rowCount()
                 self.table_product.insertRow(row)
                 for col in range(1, 13):
-                    twi = QTableWidgetItem(str(query.value(col)))
+                    if col == 9:
+                        value = query.value(col)
+                        if value == 0:
+                            value = "No"
+                        else:
+                            value = "Yes"
+                        twi = QTableWidgetItem(value)
+                    else:
+                        twi = QTableWidgetItem(str(query.value(col)))
                     self.table_product.setItem(row, col-1, twi)
         else:
             QMessageBox.critical(self, "Database Error", query.lastError().text())
 
     def event_button_add_product_clicked(self):
+        dlgAddProduct = DlgAddProduct()
+        dlgAddProduct.button_submit_product.clicked.connect(dlgAddProduct.event_button_submit_clicked)
+        dlgAddProduct.show()
+        dlgAddProduct.exec()
+        self.populateUiTableProduct()
+
+
+class DlgAddProduct(QDialog, Ui_add_product_dialog):
+    def __init__(self):
+        super(DlgAddProduct, self).__init__()
+        self.setupUi(self)
+        
+        self.button_cancel.clicked.connect(self.close)
+
+    def event_button_submit_clicked(self):
         self.validate_product_data()
         query = QSqlQuery()
         query.prepare("INSERT into product (code, brand, generic, size, class, type, price, cost, is_active, ave_per_month, high, low) VALUES(:code, :brand, :generic, :size, :class, :type, :price, :cost, :is_active, :ave_per_month, :high, :low)")
@@ -66,20 +90,21 @@ class DlgMain(QMainWindow, Ui_MainWindow):
         query.bindValue(":brand", self.value_brand_product.text())
         query.bindValue(":generic", self.value_generic_product.text())
         query.bindValue(":size", self.value_size_product.text())
-        query.bindValue(":class", self.value_class_product.text())
-        query.bindValue(":type", self.value_type_product.text())
+        query.bindValue(":class", self.value_class_product.currentText())
+        query.bindValue(":type", self.value_type_product.currentText())
         query.bindValue(":price", self.value_price_product.text())
         query.bindValue(":cost", self.value_cost_product.text())
-        query.bindValue(":is_active", self.value_is_active_product.text())
+        query.bindValue(":is_active", self.value_is_active_product.checkState())
         query.bindValue(":ave_per_month", 0)
         query.bindValue(":high", 0)
         query.bindValue(":low", 0)
         bOk = query.exec()
         if bOk:
             QMessageBox.information(self, "Successfully added", "Product was added successfully")
+            self.close()
         else:
             QMessageBox.warning(self, "Database Error", query.lastError().text())
-        self.populateUiTableProduct()
+        
 
     def validate_product_data(self):
         print("Data being validated for product")
